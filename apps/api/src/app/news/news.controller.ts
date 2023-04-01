03f593293cf7eaa17a9926922bc8a1b93e3db9c2
Alice <000>
2023-04-01 16:30:39 +0200

    homework-1

--- apps/api/src/app/news/news.controller.ts
+++ apps/api/src/app/news/news.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post } from '@nestjs/common';
+
+import { IsNotEmpty } from 'class-validator';
+
+const db = new Map();
+let resCash = [];
+
+export class CreateNewsDto {
+  @IsNotEmpty()
+  title: string;
+
+  @IsNotEmpty()
+  description: string;
+}
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+  @Get()
+  async getNews() {
+    return new Promise(resolve => {
+      if (!db.has('genNews')) {
+      const news = Object.keys([...Array(10)])
+        .map(key => Number(key) + 1)
+        .map(n => ({
+          id: n,
+          title: `Важная новость ${n}`,
+          description: `Описание новости ${n}`,
+          createdAt: Date.now()
+        }))
+        db.set('genNews', news);
+        resCash = Array.from(db.values()).flat();
+      }
+
+      setTimeout(() => {
+        resolve(resCash);
+      }, 100)
+    });
+  }
+
+  @Post()
+  @Header('Cache-Control', 'none')
+  create(@Body() peaceOfNews: CreateNewsDto) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('Новость успешно создана', peaceOfNews);
+        const item = { id: Math.ceil(Math.random() * 1000), ...peaceOfNews };
+        db.set(item.id, item);
+        resCash = Array.from(db.values()).flat();
+        resolve(item);
+      }, 100)
+    });
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,16 +3,18 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from "./app/app.module";
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3001;
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
